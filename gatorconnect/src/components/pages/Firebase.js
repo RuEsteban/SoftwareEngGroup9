4e1745daba9d98f55faaeda8d231ccb73275a214
egiro <e.giron201875@gmail.com>
2024-10-28 15:20:11 -0400

    updated with firebase connection

--- gatorconnect/src/components/pages/Firebase.js
+++ gatorconnect/src/components/pages/Firebase.js
@@ -6,7 +6,13 @@ import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-route
 
 
 const firebaseConfig = {
-   //copy paste here 
+    apiKey: "AIzaSyDJUA793eEFVUbVIG2s6rLZ0jBZBgeOEq4",
+    authDomain: "cengroup3031-d4a4c.firebaseapp.com",
+    projectId: "cengroup3031-d4a4c",
+    storageBucket: "cengroup3031-d4a4c.appspot.com",
+    messagingSenderId: "1052017859368",
+    appId: "1:1052017859368:web:ddf61ec493f1c41c4cce1d",
+    measurementId: "G-BYVCY72X95" 
   };
 
 // Initialize Firebase
